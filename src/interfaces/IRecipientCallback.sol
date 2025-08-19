ae668f5f61d8dead0eb791f045ad9b5caaa37020
0age <37939117+0age@users.noreply.github.com>
2025-08-19 11:58:58 -0700

    put it back lowercase

---
+++ src/interfaces/IRecipientCallback.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {BatchCompact} from "../types/TribunalStructs.sol";
+
+interface IRecipientCallback {
+    /// @notice Callback function to be called by the Tribunal contract to the recipient.
+    /// @dev At this point, the filler has received the fillToken with an amount of actualFillAmount.
+    /// Must return this function selector.
+    /// @param sourceClaimHash The claim hash of the compact that has been claimed.
+    /// @param sourceMandateHash The mandate hash of the compact that has been claimed.
+    /// @param fillToken The reward token of the fill.
+    /// @param fillAmount The actual fill amount that is provided.
+    /// @param targetCompact The new compact associated with the callback.
+    /// @param targetMandateHash The mandate hash associated with the new compact.
+    /// @param context Arbitrary context associated with the callback.
+    function tribunalCallback(
+        uint256 chainId,
+        bytes32 sourceClaimHash,
+        bytes32 sourceMandateHash,
+        address fillToken,
+        uint256 fillAmount,
+        BatchCompact calldata targetCompact,
+        bytes32 targetMandateHash,
+        bytes calldata context
+    ) external returns (bytes4);
+}
