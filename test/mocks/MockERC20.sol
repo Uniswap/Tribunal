305fc4fd649211b0e93254ef70a359469db514fd
0age <37939117+0age@users.noreply.github.com>
2025-10-28 14:24:49 -0700

    add some tests

--- test/mocks/MockERC20.sol
+++ test/mocks/MockERC20.sol
@@ -15,4 +15,8 @@ contract MockERC20 is ERC20 {
     function symbol() public pure override returns (string memory) {
         return "MOCK";
     }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
 }
